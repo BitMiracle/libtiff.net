6e6e348b423aa264e2e7cbe96edd5ee0dc93978f
Sergey Bobrovsky <sergius.bobrovsky@gmail.com>
2009-06-10 22:22:59 +0000

    added license and readme

--- LibJpeg.NET/LibJpeg/Properties/AssemblyInfo.cs
+++ LibJpeg.NET/LibJpeg/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("LibJpeg")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("BitMiracle.LibJpeg.NET")]
+[assembly: AssemblyDescription(".NET version of IJG's LibJpeg library made by Bit Miracle")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("LibJpeg")]
-[assembly: AssemblyCopyright("Copyright ©  2009")]
+[assembly: AssemblyCompany("Bit Miracle")]
+[assembly: AssemblyProduct("BitMiracle.LibJpeg.NET")]
+[assembly: AssemblyCopyright("Copyright (C) 2008-2009, Bit Miracle")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -21,16 +21,3 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("0614c098-c932-4bb7-998e-8b1cbc546871")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
