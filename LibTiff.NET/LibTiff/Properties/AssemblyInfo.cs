974ddc7a957ee183d06b3104691e773512397b29
Sergey Bobrovsky <sergius.bobrovsky@gmail.com>
2009-10-10 22:02:37 +0000

    -

---
+++ LibTiff.NET/LibTiff/Properties/AssemblyInfo.cs
@@ -0,0 +1,23 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("BitMiracle.LibTiff.NET")]
+[assembly: AssemblyDescription(".NET version of LibTiff library made by Bit Miracle")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Bit Miracle")]
+[assembly: AssemblyProduct("BitMiracle.LibTiff.NET")]
+[assembly: AssemblyCopyright("Copyright (C) 2008-2009, Bit Miracle")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("c4570b0f-2eeb-428b-b55c-8fa225621e86")]
