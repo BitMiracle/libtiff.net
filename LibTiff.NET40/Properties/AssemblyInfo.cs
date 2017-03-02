41bf7980f3258b45c6b3c1a198e63f16a3f15a7c
gpetrou <gpetrou@actify.com>
2017-03-02 17:42:57 +0000

    Added .NET 4.0 project

---
+++ LibTiff.NET40/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("BitMiracle.LibTiff.NET")]
+[assembly: AssemblyDescription(".NET 4.0 version of LibTiff library made by Bit Miracle")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Bit Miracle")]
+[assembly: AssemblyProduct("BitMiracle.LibTiff.NET")]
+[assembly: AssemblyCopyright("Copyright (C) 2008-2017, Bit Miracle")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("5f3f6e2b-c322-4492-b8dd-dab23377670f")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
