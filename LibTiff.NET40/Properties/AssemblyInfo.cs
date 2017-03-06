fb34c221ab7507cc2727c6c39e381a9b8e9ee72a
Sergey Bobrovsky <sergius.bobrovsky@gmail.com>
2017-03-06 23:45:52 +0600

    .NET 4 project fixes

--- LibTiff.NET40/Properties/AssemblyInfo.cs
+++ LibTiff.NET40/Properties/AssemblyInfo.cs
@@ -1,4 +1,5 @@
-﻿using System.Reflection;
+﻿using System;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
@@ -22,15 +23,4 @@ using System.Runtime.InteropServices;
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("5f3f6e2b-c322-4492-b8dd-dab23377670f")]
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: CLSCompliant(true)]
