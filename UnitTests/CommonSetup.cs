a333324f18774fa295942086a63c26f0c9d3b619
Sergey Bobrovsky <sergius.bobrovsky@gmail.com>
2021-12-19 22:56:33 +0600

    Added tests setup. This fixes execution of tests in Visual Studio 2022 Test Explorer

---
+++ UnitTests/CommonSetup.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+
+using System;
+using System.IO;
+
+namespace UnitTests
+{
+    [SetUpFixture]
+    public class CommonSetup
+    {
+        [OneTimeSetUp]
+        public void RunBeforeAnyTests()
+        {
+            var dir = Path.GetDirectoryName(typeof(CommonSetup).Assembly.Location);
+            Environment.CurrentDirectory = dir;
+        }
+    }
+}
